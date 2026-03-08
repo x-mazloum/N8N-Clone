32a7f814005ad386be7a7431e40bed3cbc2e1aa8
x-mazloum <mohammadmazloum603@gmail.com>
2026-03-08 23:10:47 +0200

    15-editor

---
+++ src/components/workflow-node.tsx
@@ -0,0 +1,56 @@
+import { NodeToolbar, Position } from "@xyflow/react";
+import { SettingsIcon, TrashIcon } from "lucide-react";
+import type { ReactNode } from "react";
+import { Button } from "./ui/button";
+
+
+interface WorkflowNodeProps {
+    children: ReactNode;
+    showToolbar?: boolean;
+    onDelete?: () => void;
+    onSettings?: () => void;
+    name?: string;
+    description?: string;
+};
+
+export function WorkflowNode ({
+    children,
+    showToolbar = true,
+    onDelete,
+    onSettings,
+    name,
+    description
+}: WorkflowNodeProps)
+{
+    return(
+        <>
+            {showToolbar && (
+                <NodeToolbar>
+                    <Button size="sm" variant="ghost" onClick={onSettings}>
+                        <SettingsIcon className="size-4" />
+                    </Button>
+                    <Button size="sm" variant="ghost" onClick={onDelete}>
+                        <TrashIcon className="size-4" />
+                    </Button>
+                </NodeToolbar>
+            )}
+            {children}
+            {name && (
+                <NodeToolbar
+                    position={Position.Bottom}
+                    isVisible
+                    className="max-w-[200px] text-center"
+                >
+                    <p className="font-medium">
+                        {name}
+                    </p>
+                    {description && (
+                        <div className="text-muted-foreground truncate text-sm">
+                            {description}
+                        </div>
+                    )}
+                </NodeToolbar>
+            )}
+        </>
+    )
+}
