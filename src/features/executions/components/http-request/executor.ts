e56e49c7f3ab357f4858a84fd8d8a3d50af6e0a0
x-mazloum <mohammadmazloum603@gmail.com>
2026-03-28 18:55:42 +0200

    19: variable name added for http request executor

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -3,6 +3,7 @@ import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions } from "ky";
 
 type HttpRequestData = {
+    variableName?: string;
     endpoint?: string;
     method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
     body?: string;
@@ -19,6 +20,10 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async({
     // Publish "Error" state for http request
         throw new NonRetriableError("Http Request node: No endpoint configured");
     }
+    if(!data.variableName){
+    // Publish "Error" state for http request
+        throw new NonRetriableError("Variable name not configured!");
+    }
     const result = await step.run("http-request", async () => {
         const endpoint = data.endpoint!;
         const method = data.method || "GET";
@@ -27,6 +32,9 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async({
 
         if(["POST", "PUT", "PATCH"].includes(method)){
             options.body = data.body;
+            options.headers = {
+                "Content-Type": "application/json",
+            }
         }
         const response = await ky(endpoint, options);
         const contentType = response.headers.get("content-type");
@@ -34,16 +42,29 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async({
                     ? await response.json()
                     : await response.text();
 
-        return {
-            ...context,
+        const responsePayload = {
             httpResponse: {
                 status: response.status,
                 statusText: response.statusText,
                 data: responseData,
             }
         }
+
+        if(data.variableName){
+            return {
+                ...context,
+                [data.variableName] : responsePayload,
+            }
+        }
+        // Fallback to direct httpResponse for backward compatibility
+        return{
+            ...context,
+            ...responsePayload,
+        };
     })
     // Publish "success" state for http request
 
     return result;
 }
+// there was a bug when executing that the last node overrides the nodes before so only the data of the last node is displayed in inngest(key collision)
+// this approach of the variable name fixed this bug explain it in details
