0072f52ba0b3c4facfd77027fa63d2ceaf291573
x-mazloum <mohammadmazloum603@gmail.com>
2026-03-30 17:49:28 +0300

    23: stripe-trigger

--- src/config/node-components.ts
+++ src/config/node-components.ts
@@ -2,6 +2,7 @@ import { InitialNode } from "@/components/initial-node";
 import { HttpRequestNode } from "@/features/executions/components/http-request/node";
 import { GoogleFormTrigger } from "@/features/triggers/components/google-form-trigger/node";
 import { ManualTriggerNode } from "@/features/triggers/components/manual-trigger/node";
+import { StripeTriggerNode } from "@/features/triggers/components/stripe-trigger/node";
 import  { NodeType } from "@/generated/prisma/enums";
 import type { NodeTypes } from "@xyflow/react";
 
@@ -10,6 +11,7 @@ export const nodeComponents = {
     [NodeType.HTTP_REQUEST]: HttpRequestNode,
     [NodeType.MANUAL_TRIGGER]: ManualTriggerNode,
     [NodeType.GOOGLE_FORM_TRIGGER]: GoogleFormTrigger,
+    [NodeType.STRIPE_TRIGGER] : StripeTriggerNode,
 } as const satisfies NodeTypes
 
 export type RegisteredNodeType = keyof typeof nodeComponents;
