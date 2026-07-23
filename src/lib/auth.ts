feab4477cd27b3b88b5543637d0e0724ef739e1f
x-mazloum <mohammadmazloum603@gmail.com>
2026-07-23 18:44:45 +0300

    fixed biome

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,9 +4,6 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
 // import { polarClient } from "./polar";
 
-
-
-
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
@@ -26,23 +23,22 @@ export const auth = betterAuth({
     },
   },
   plugins: [],
-    
-    // polar({
-    //   client: polarClient,
-    //   createCustomerOnSignUp: true,
-    //   use: [
-    //     checkout({
-    //       products: [
-    //         {
-    //           productId: "034dcfe6-092f-41cd-ae08-6610c067e498",
-    //           slug: "Nodebase-Pro",
-    //         },
-    //       ],
-    //       successUrl: process.env.POLAR_SUCCESS_URL,
-    //       authenticatedUsersOnly: true,
-    //     }),
-    //     portal(),
-    //   ],
-    // }),
-  
+
+  // polar({
+  //   client: polarClient,
+  //   createCustomerOnSignUp: true,
+  //   use: [
+  //     checkout({
+  //       products: [
+  //         {
+  //           productId: "034dcfe6-092f-41cd-ae08-6610c067e498",
+  //           slug: "Nodebase-Pro",
+  //         },
+  //       ],
+  //       successUrl: process.env.POLAR_SUCCESS_URL,
+  //       authenticatedUsersOnly: true,
+  //     }),
+  //     portal(),
+  //   ],
+  // }),
 });
